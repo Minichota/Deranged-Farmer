101a5c85c2c057836df5ea3ee3b08bc68220bb3b
Ouj <minichota1219@gmail.com>
2019-09-29 13:13:57 -0500

    Introduced game state abstraction class.

---
+++ src/game-state/game-state.cpp
@@ -0,0 +1,10 @@
+#include "game-state.hpp"
+
+GameState::GameState(SDL_Texture* world_texture):
+Renderable(world_texture)
+{
+}
+
+GameState::~GameState()
+{
+}
---
+++ src/game-state/game-state.hpp
@@ -0,0 +1,10 @@
+#include "renderable.hpp"
+
+class GameState : public Renderable {
+	public:
+	GameState(SDL_Texture* world_texture);
+	virtual ~GameState();
+
+	virtual void update() = 0;
+	virtual void render() = 0;
+};
