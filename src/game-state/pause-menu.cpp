a99a2c1ce71e22178161107f73ccf391a4054ad5
Ouj <minichota1219@gmail.com>
2019-10-27 15:51:49 -0500

    Removed excess pause-menu element

--- src/game-state/pause-menu.cpp
+++ src/game-state/pause-menu.cpp
@@ -25,20 +25,18 @@ void Pause_Menu::init()
 	{
 		new UI_Text(renderer, Ivec(400,200), Ivec(300,150), Fvec(1.0f,1.0f), "PAUSED", "res/graphics/font.ttf", SDL_Color{255,255,255,255}, FILL),
 		new UI_Text(renderer, Ivec(400,500), Ivec(0,0), Fvec(1.0f,1.0f), "Press enter to continue", "res/graphics/font.ttf", SDL_Color{255,255,255,255}, NORMAL),
-		new UI_Button(renderer, Ivec(0,0), Ivec(0,0), Fvec(1.0f,1.0f), &quit),
+		new UI_Button(renderer, Ivec(0,0), Ivec(0,0), Fvec(2.0f,2.0f), &quit, SDL_Color{255,255,255,255}),
 		new UI_Text(renderer, Ivec(0,0), Ivec(0,0), Fvec(1.0f,1.0f), "Main Menu", "res/graphics/font.ttf", SDL_Color{255,255,255,255}, NORMAL),
-		new UI_Rect(renderer, Ivec(0,0), Ivec(0,0), Fvec(2.0f,2.0f), SDL_Color{255,255,255,255},NORMAL),
 		new UI_Image(renderer, Ivec(735,550), Ivec(50,50), Fvec(1.0f,1.0f), FILL, "res/graphics/volume.png"),
 		new UI_Slider(renderer, Ivec(735,580), Ivec(110,10), Fvec(1.0f,1.0f), 0, 100, 100, SDL_Color{255,255,255,255}, SDL_Color{127,127,127,255})
 	};
 	elements[0]->set_origin(elements[0]->get_size()/2);
 	elements[1]->set_origin(elements[1]->get_size()/2);
-	elements[2]->set_size(elements[3]->get_size());
-	elements[4]->set_size(elements[3]->get_size()/2);
+	elements[2]->set_size(elements[3]->get_size()/2);
+	elements[4]->set_origin(elements[4]->get_size()/2);
 	elements[5]->set_origin(elements[5]->get_size()/2);
-	elements[6]->set_origin(elements[6]->get_size()/2);
 
-	dynamic_cast<UI_Slider*>(elements[6])->set_bind(&Settings::volume);
+	dynamic_cast<UI_Slider*>(elements[5])->set_bind(&Settings::volume);
 
 	uchar_interpolaters.push_back(Interpolator<unsigned char>{&elements[1]->get_color().a,0,255,750,750});
 }
