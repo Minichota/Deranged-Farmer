823a3f534ceff38a2c48510c7bcc1417ff77ecfa
Ouj <minichota1219@gmail.com>
2019-11-09 20:32:37 -0600

    (#53) Added Map_Entity debug value

--- src/entity/map/map-entity.cpp
+++ src/entity/map/map-entity.cpp
@@ -1,4 +1,5 @@
 #include "map-entity.hpp"
+#include "game.hpp"
 
 Map_Entity::Map_Entity(SDL_Renderer* renderer, Fvec pos, Fvec size):
 Entity(renderer, pos, size)
@@ -22,6 +23,11 @@ void Map_Entity::render()
 		(int)std::round(size.x),
 		(int)std::round(size.y)
 	};
+	if(Game::debug->active)
+	{
+		Game::debug->push_render(this, "Entity", {&pos.x, &pos.x,
+													  &size.x, &size.y});
+	}
 	SDL_RenderCopy(renderer, texture, NULL, &output_rect);
 }
 
